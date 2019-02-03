6c816be673fd81174e995694be28d8f17c5349d1
WilliamUng <william.ung@live.com>
2019-02-03 15:08:17 -0500

    error messages added

--- app/src/main/java/com/example/mortgagecalculator/MainActivity.java
+++ app/src/main/java/com/example/mortgagecalculator/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,17 +44,27 @@ public class MainActivity extends AppCompatActivity {
         EditText interest = (EditText)findViewById(R.id.editText2);
         Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
 
-        String prin = principal.getText().toString();
-        String inter = interest.getText().toString();
-        String period = mySpinner.getSelectedItem().toString();
+        if(principal.getText().toString().matches("") || interest.getText().toString().matches("") || mySpinner.getSelectedItem().toString().matches("")) {
+            Toast.makeText(getApplicationContext(), "Missing information", Toast.LENGTH_LONG).show();
+        }
+        else if (!(Integer.parseInt(interest.getText().toString()) <= 30 && Integer.parseInt(interest.getText().toString()) > 0))
+        {
+            Toast.makeText(getApplicationContext(), "The interest rate must be between 0% and 30%.", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            String prin = principal.getText().toString();
+            String inter = interest.getText().toString();
+            String period = mySpinner.getSelectedItem().toString();
 
-        Intent summary = new Intent (this, SummaryPage.class);
+            Intent summary = new Intent(this, SummaryPage.class);
 
-        summary.putExtra("principal", prin);
-        summary.putExtra("interest", inter);
-        summary.putExtra("period", period);
+            summary.putExtra("principal", prin);
+            summary.putExtra("interest", inter);
+            summary.putExtra("period", period);
 
-        startActivity(summary);
+            startActivity(summary);
+        }
     }
 
     public void Help (View v) {
